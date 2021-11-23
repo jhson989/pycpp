c0f9dac0e1d3513f692ad4efdc37022dac25fb98
Janghyun Son <jhson989@gmail.com>
2021-11-23 13:59:10 +0900

    Add performance measurement

--- exIntegrate/integrate.pyx
+++ exIntegrate/integrate.pyx
@@ -1,15 +1,27 @@
 
 # except? -2 means that an error will be checked for if -2 is returned 
 # (though the ? indicates that -2 may also be used as a valid return value)
-cdef double f(double x) except? -2:
-    return x**2 - x
+cdef double f_typed(double x) except? -2:
+    return x**2 - 2*x
 
-cpdef double integrate(double a, double b, int N=1):
+cpdef double integrate_typed(double a, double b, int N=1):
     cdef int i
     cdef double s = 0
     cdef double dx = (b-a)/N
     
     for i in range (N):
-        s += (f(a+i*dx)+f(a+(i+1)*dx))/2 * dx
+        s += (f_typed(a+i*dx)+f_typed(a+(i+1)*dx))/2 * dx
+    
+    return s
+
+
+def f_untyped(x):
+    return x**2 - 2*x
+
+def integrate_untyped(a, b, N=1):
+    s = 0
+    dx = (b-a)/N
+    for i in range (N):
+        s += (f_untyped(a+i*dx)+f_untyped(a+(i+1)*dx))/2 * dx
     
     return s
--- exIntegrate/main.py
+++ exIntegrate/main.py
@@ -1,13 +1,37 @@
-from integrate import integrate
+from integrate import integrate_typed, integrate_untyped
 import time
 
 
+def f(x):
+    return x**2 - 2*x
 
+def integrate_python(a, b, N=1):
+    s = 0
+    dx = (b-a)/N
+    for i in range (N):
+        s += (f(a+i*dx)+f(a+(i+1)*dx))/2 * dx
+    
+    return s
 if __name__ == "__main__":
 
-    for i in range(4, 9):
+    print("\nCython performance test")
+
+    for i in range(6, 9):
+        
+        print("Divide into 1e+%d" % i)
+
         start = time.time_ns() 
-        result = integrate(-100, 100, int(10**i))
-        end = time.time_ns()
-        print("1e+%d : %f (%fs)" % (i, result, (end-start)*1e-9))
-    
+        result_python = integrate_python(-50, 100, int(10**i))
+        time_python = time.time_ns() - start
+        
+        start = time.time_ns() 
+        result_untyped = integrate_untyped(-50, 100, int(10**i))
+        time_untyped = time.time_ns() - start
+
+        start = time.time_ns() 
+        result_typed = integrate_typed(-50, 100, int(10**i))
+        time_typed = time.time_ns() - start
+
+        print("Result : %f = %f = %f" % (result_python, result_untyped, result_typed))
+        print("Elaped time: python(%fs) untyped cython(%fs) typed cython(%fs)" % (time_python*1e-9, time_untyped*1e-9, time_typed*1e-9))
+        print("Speed up : untyped cython(%f), typed cython(%f)" % (time_python/time_untyped, time_python/time_typed))
