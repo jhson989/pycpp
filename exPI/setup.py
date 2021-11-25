673db90eb709180c9757767a055843c5b45e0c2a
Janghyun Son <jhson989@gmail.com>
2021-11-25 15:17:32 +0900

    Clean up the build configuration

--- exPI/setup.py
+++ exPI/setup.py
@@ -1,9 +1,15 @@
-from distutils.core import setup
+from distutils.core import Extension, setup
 from Cython.Build import cythonize
+import pathlib
 
-setup(
-    name="Monte Carlo PI calculator",
-    ext_modules=cythonize(
-        ["src/pyInterface.pyx"]
-    , annotate=True)
-)
+ext_modules = [
+    Extension("pyInterface",
+              sources=["pyInterface.pyx"],
+              libraries=["piCalculator"],
+              library_dirs=[str(pathlib.Path().resolve())],
+              include_dirs=[str(pathlib.Path().resolve())+"/../../include/"]
+              )
+]
+
+setup(name="pyInterface",
+      ext_modules=cythonize(ext_modules))
